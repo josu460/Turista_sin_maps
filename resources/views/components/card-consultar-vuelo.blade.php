f880f5743c86444af82c1e43abda86508a3706c3
Josue Rivera <122042206@upq.edu.mx>
2024-11-05 01:23:00 -0600

    cambios card consultar vuelo

--- resources/views/components/card-consultar-vuelo.blade.php
+++ resources/views/components/card-consultar-vuelo.blade.php
@@ -4,24 +4,31 @@
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Vuelo a Irlanda
         </h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p class="mb-1 font-normal text-gray-700 dark:text-gray-400">
             Partiendo desde Ciudad de México
         </p>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p class="mb-1 font-normal text-gray-700 dark:text-gray-400">
             Por Aeromexico
         </p>
-        <div>
-            <p class="mb-3 font-bold text-gray-700 dark:text-gray-400">
-                $10,000
-            </p>
-        </div>
+        <p class="mb-1 font-normal text-gray-700 dark:text-gray-400">
+            04/12/2024 - 06/12/2024
+        </p>
+        <p class="mb-1 font-bold text-gray-700 dark:text-gray-400">
+            $10,000 MXN
+        </p>
+        <p class="mb-5  font-normal text-gray-700 dark:text-gray-400">
+            Vuelo redondo
+        </p>
+
         <div class="flex space-x-8 ">
             <a href="#" class="inline-flex items-center px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Editar
             </a>
-            <a href="#" class="inline-flex items-center px-5 py-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+            <a href="#" data-modal-target="delete-modal" data-modal-toggle="delete-modal" class="inline-flex items-center px-5 py-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                 Eliminar
             </a>
+
+
         </div>
     </div>
 </div>
