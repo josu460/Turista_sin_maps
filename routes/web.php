fc4e57f1786473f55d42e733e2625dbfae4b1cef
Josue Rivera <122042206@upq.edu.mx>
2024-11-06 14:36:43 -0600

    web con angel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\controladorVistas;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +18,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/rutasjosue.php';
+require __DIR__.'/rutascristopher.php';
